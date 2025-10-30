3d38163d2b80f1a47f4fed4b430298d828d0ce29
Dusk <1797578700@qq.com>
2025-10-30 17:10:49 +0800

    1

---
+++ 集合差集运算.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+#define OK 1
+#define ERROR -1
+#define MAXSIZE 100
+typedef int ElemType;
+typedef int Status;
+
+
+typedef struct LNode
+{
+	ElemType* elem;
+	int length;
+}Sqlist;
+
+Status InitLNode(Sqlist& L)
+{
+	L.elem = new ElemType[MAXSIZE];
+	if (!L.elem)
+		return ERROR;
+	L.elem = nullptr;
+	return OK;
+}
+
+Status GetElem(Sqlist L,int i,ElemType e)
+{
+	if (i<1 || i>L.length)
+	{
+		return ERROR;
+	}
+	e = L.elem[i - 1];
+	return OK;
+}
+
+Status LocateElem(Sqlist L,int i, ElemType e)
+{
+	for (i = 0; i <= L.length; i++)
+	{
+		if (L.elem[i - 1] == e)
+		{
+			e = L.elem[i - 1];
+		}
+	}
+	return 0;
+}
+
+Status ListInsert(Sqlist L, int i, ElemType e)
+{
+	if (i<1 || i>L.length + 1)return ERROR;
+	if (L.length >= MAXSIZE)return ERROR;
+	for (int j = L.length - 1; j <= i; --j)
+	{
+		L.elem[j + 1] = L.elem[j];
+	}
+	L.elem[i - 1] = e;
+	++L.length;
+	return OK;
+}
+
